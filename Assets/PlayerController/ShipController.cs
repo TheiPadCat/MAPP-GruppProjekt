f5b5b0e5516e845215352599a8bc989135f14b4c
linpack12 <55255203+linpack12@users.noreply.github.com>
2023-04-15 17:48:08 +0200

    fixat lite med speed curven

--- Assets/PlayerController/ShipController.cs
+++ Assets/PlayerController/ShipController.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
-    
-    public float maxSpeed = 15f; // Speed of the ship
+    public float maxSpeed = 15f;
     public AnimationCurve speedCurve;
     public float speedMultiplier = 1f;
     public float speedWhenMouseOver = 0f;
@@ -15,50 +14,48 @@ public class ShipController : MonoBehaviour
     public float dashCooldown = 2f;
     private float lastDashTime;
 
-    public Vector2 mousePos; // Position of the mouse cursor
-    private Rigidbody2D rb; // Rigidbody of the ship
+    public Vector2 mousePos;
+    private Rigidbody2D rb;
 
     private void Start()
     {
-        
-        rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody component
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Get the mouse position in world space
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        if(Input.GetKeyDown(KeyCode.Space) && Time.time > lastDashTime + dashCooldown)
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > lastDashTime + dashCooldown)
         {
-            StartCoroutine(DashCorutine());
+            StartCoroutine(DashCoroutine());
             Debug.Log("Dash");
         }
     }
 
     private void FixedUpdate()
     {
-        Vector2 direction = mousePos - rb.position; // Calculate direction towards the mouse cursor
+        Vector2 direction = mousePos - rb.position;
 
         float distance = Vector2.Distance(mousePos, rb.position);
         float speed = speedCurve.Evaluate(distance) * speedMultiplier;
 
-        if(distance < mouseDetectionRadius)
+        if (distance < mouseDetectionRadius)
         {
             speed = speedWhenMouseOver;
         }
-        rb.velocity = direction.normalized * speed; // Move the ship towards the mouse cursor
 
-        // Rotate the ship to face the direction of movement
+        rb.velocity = direction.normalized * speed * maxSpeed;
+
         float angle = Mathf.Atan2(direction.x, -direction.y) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
-    private IEnumerator DashCorutine()
+    private IEnumerator DashCoroutine()
     {
         rb.AddForce(transform.up * dashForce, ForceMode2D.Impulse);
         yield return new WaitForSeconds(dashDuration);
 
-        // Set the velocity to the current speed of the ship
         rb.velocity = rb.velocity.normalized * Mathf.Clamp(rb.velocity.magnitude, 0f, maxSpeed);
         lastDashTime = Time.time;
     }
