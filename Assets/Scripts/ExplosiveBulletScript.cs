b924bc3c6abf553f3083e78196262314eaf96443
lovacarlsson <112973903+lovacarlsson@users.noreply.github.com>
2023-04-19 13:03:46 +0200

    ExplosiveTurret

--- Assets/Scripts/ExplosiveBulletScript.cs
+++ Assets/Scripts/ExplosiveBulletScript.cs
@@ -6,9 +6,10 @@ using UnityEngine.UIElements;
 public class ExplosiveBulletScript: MonoBehaviour
 {
     [SerializeField] float bulletVelocity;
+    [SerializeField] private float shatterForce = 20f;
+    [SerializeField] private int shatterTime = 2;
     private int amountOfShatters;
-    private float shatterMoveForce;
-
+  
     private bool hasInstantiated;
 
     private Transform target;
@@ -22,19 +23,21 @@ public class ExplosiveBulletScript: MonoBehaviour
 
     private LayerMask test;
 
+    private GameObject bomb;
+
 
     void Start()
     {
         //Detta borde flyttas till en manager
         Physics2D.IgnoreLayerCollision(6, 8);
 
-
+       
         GetComponent<Rigidbody2D>().velocity = direction * bulletVelocity;
 
     }
 
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision) /// TO DO: FIXA METODER / LOOPAR IST FÖR BA DUMPA KOD
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
@@ -42,21 +45,58 @@ public class ExplosiveBulletScript: MonoBehaviour
 
             if (!hasInstantiated) //Kontrollera att vi inte spawnar för många instanser
             {
-                GameObject bomb = Instantiate(explosivePrefab, spawnPosition, Quaternion.identity);
-
+                //Skapa instanser av alla bomber
+               
+               GameObject bomb1 = Instantiate(explosivePrefab, spawnPosition, Quaternion.identity);
+               GameObject bomb2 = Instantiate(explosivePrefab, spawnPosition, Quaternion.identity);
+               GameObject bomb3 = Instantiate(explosivePrefab, spawnPosition, Quaternion.identity);
+               GameObject bomb4 = Instantiate(explosivePrefab, spawnPosition, Quaternion.identity);
                 hasInstantiated = true;
 
                 //Ta bort Bullet objektets barn
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0) ;
                 DisableChildren();
 
-                Rigidbody2D bombRb = bomb.GetComponent<Rigidbody2D>();
+                //Hämta alla rigidbody
+                Rigidbody2D bomb1Rb = bomb1.GetComponent<Rigidbody2D>();
+                Rigidbody2D bomb2Rb = bomb2.GetComponent<Rigidbody2D>();
+                Rigidbody2D bomb3Rb = bomb3.GetComponent<Rigidbody2D>();
+                Rigidbody2D bomb4Rb = bomb4.GetComponent<Rigidbody2D>();
 
-                bombRb.AddForce(direction * shatterMoveForce);
+                //Skjuta alla åt varsitt håll
+                bomb1Rb.AddForce(new Vector3(shatterForce, 0) * shatterForce, ForceMode2D.Impulse);
+                bomb2Rb.AddForce(new Vector3(0, shatterForce) * shatterForce, ForceMode2D.Impulse);
+                bomb3Rb.AddForce(new Vector3(-shatterForce, 0) * shatterForce, ForceMode2D.Impulse);
+                bomb4Rb.AddForce(new Vector3(0, -shatterForce) * shatterForce, ForceMode2D.Impulse);
+
+                //Sprängas efter 1 sekund
+
+                StartCoroutine(shatterAfterTime(bomb1));
+                StartCoroutine(shatterAfterTime(bomb2));
+                StartCoroutine(shatterAfterTime(bomb3));
+                StartCoroutine(shatterAfterTime(bomb4));
+
+                //Göra damage till fiender
+
 
             }
 
-            //StartCoroutine(SpawnShatterAndDelete());
+          
         }
+    }
+
+    IEnumerator shatterAfterTime(GameObject bombToExplode)
+    {
+        yield return new WaitForSeconds(shatterTime);
+        //Sprängas
+        print(bombToExplode + " sprängs");
+
+        yield return new WaitForSeconds(1);
+        
+        Destroy(gameObject);
+        Destroy(bombToExplode);
+
+        print("Förstör bomb + bullet");
     }
 
 
@@ -66,5 +106,7 @@ public class ExplosiveBulletScript: MonoBehaviour
             child.gameObject.SetActive(false);
     }
    
+
+    
 
 }
