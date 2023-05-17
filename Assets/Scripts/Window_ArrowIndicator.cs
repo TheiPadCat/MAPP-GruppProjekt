261e9166a9924c7d589b5076809eb3169675b728
linpack12 <55255203+linpack12@users.noreply.github.com>
2023-05-17 18:46:43 +0200

    arrowIndicatorScript typklart

--- Assets/Scripts/Window_ArrowIndicator.cs
+++ Assets/Scripts/Window_ArrowIndicator.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey.Utils;
 
-public class Window_ : MonoBehaviour
+public class Window_ArrowIndicator : MonoBehaviour
 {
+    [SerializeField] Camera uiCamera;
     private Vector3 targetPosition;
     private RectTransform poinerRectTransform;
 
@@ -23,9 +24,41 @@ public class Window_ : MonoBehaviour
         float angle = UtilsClass.GetAngleFromVector(direction);
         poinerRectTransform.localEulerAngles = new Vector3(0, 0, angle);
 
+        float borderSize = 100f;
         Vector3 targetPosScreenPoint = Camera.main.WorldToScreenPoint(targetPosition);
-        bool isOffScreen = targetPosScreenPoint.x <= 0 || targetPosScreenPoint.x >= Screen.width || targetPosScreenPoint.y <= 0 || targetPosScreenPoint.y >= Screen.height;
+        bool isOffScreen = targetPosScreenPoint.x <= borderSize || targetPosScreenPoint.x >= Screen.width - borderSize || targetPosScreenPoint.y <= borderSize || targetPosScreenPoint.y >= Screen.height - borderSize;
         Debug.Log(isOffScreen + " " + targetPosScreenPoint);
 
+        if (isOffScreen)
+        {
+            Vector3 cappedTargetScreenPos = targetPosScreenPoint;
+            if(cappedTargetScreenPos.x <= borderSize)
+            {
+                cappedTargetScreenPos.x = borderSize;
+            }
+            if(cappedTargetScreenPos.x >= Screen.width - borderSize)
+            {
+                cappedTargetScreenPos.x = Screen.width - borderSize;
+            }
+            if(cappedTargetScreenPos.y <= borderSize)
+            {
+                cappedTargetScreenPos.y = borderSize;
+            }
+            if(cappedTargetScreenPos.y >= Screen.height - borderSize)
+            {
+                cappedTargetScreenPos.y = Screen.height - borderSize;
+            }
+
+            Vector3 pointerWorldPosition = uiCamera.ScreenToWorldPoint(cappedTargetScreenPos);
+            poinerRectTransform.position = pointerWorldPosition;
+            poinerRectTransform.localPosition = new Vector3(poinerRectTransform.localPosition.x, poinerRectTransform.localPosition.y, 0f);
+
+        }
+        else
+        {
+            Vector3 pointerWorldPos = uiCamera.ScreenToWorldPoint(targetPosScreenPoint);
+            poinerRectTransform.position = pointerWorldPos;
+            poinerRectTransform.localPosition = new Vector3(poinerRectTransform.localPosition.x, poinerRectTransform.localPosition.y, 0f);
+        }
     }
 }
